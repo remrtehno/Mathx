f1bcbd5b7f7b508054945b2f00fba884106556c1
User <user@MacBook-Air-User.local>
2020-01-13 00:38:06 +0500

    modified htaccess/app.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -49,14 +49,17 @@ $( document ).ready(function() {
     }
 
 
-    MathJax.Hub.Config({
-        extensions: ["tex2jax.js"],
-        jax: ["input/TeX", "output/HTML-CSS"],
-        tex2jax: {
-            inlineMath: [
-                ["$", "$"],
-                ["\\(", "\\)"]
-            ]
-        }
-    });
+    if(window.MathJax) {
+        MathJax.Hub.Config({
+            extensions: ["tex2jax.js"],
+            jax: ["input/TeX", "output/HTML-CSS"],
+            tex2jax: {
+                inlineMath: [
+                    ["$", "$"],
+                    ["\\(", "\\)"]
+                ]
+            }
+        });
+    }
+
 });
