e3b10efa0ad82dff76f046c0c8260adc2762b9bb
User <user@MacBook-Air-User.local>
2020-01-15 01:16:17 +0500

    added huck 145 line in dashboard controller  = 'A1.1'

--- app/Http/Controllers/User/Dashboard.php
+++ app/Http/Controllers/User/Dashboard.php
@@ -34,9 +34,6 @@ class Dashboard extends Controller{
 			$end_test = $users->first()->end_test;
 			$start_test = $users->first()->start_test;
 			
-			if(empty($end_test)) {
-				$end_test = 10;
-			}
 			
 			if(!empty($end_test)) {
 				$more_5h = ($timenow - $end_test) >= 18000;
@@ -84,6 +81,7 @@ class Dashboard extends Controller{
 				DB::table('users')->where('id', $user_id)->update(['start_test' => $start_test, ]);
 				
 			} else {
+				print_r('m');
 				session(['alert' => 'No works condition.']);
 			}
 			
@@ -130,8 +128,6 @@ class Dashboard extends Controller{
 				$this->time_left = 3641;
 			}
 			
-			$this->allow_tests = null;
-			$this->continue_tests = null;
 			
 			$users = DB::table('users')->where('id', '=', $user_id )->get();
 			$name_lvl_table = DB::table('levels_tests')->where('level', '=', $users->first()->level_test )->get();
@@ -145,7 +141,15 @@ class Dashboard extends Controller{
 				$get_test = $flights->get()->toArray();
 			}
 			
-			return view('user.dashboard.test-container', ['users' => $users->first(), 'data' => $get_test, 'jsonData' => $get_test_json, 'time_left' => $this->time_left, 'id_test' => $name_lvl_table->first()->name_db,]);
+			
+			// huck might be a refactored
+			$id_test = 'A1.1';
+			if(!empty($name_lvl_table)) {
+				$id_test = $name_lvl_table->first()->name_db;
+			}
+			
+
+			return view('user.dashboard.test-container', ['users' => $users->first(), 'data' => $get_test, 'jsonData' => $get_test_json, 'time_left' => $this->time_left, 'id_test' => $id_test,]);
 		}
 		
 		return view('signup');
