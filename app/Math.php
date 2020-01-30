98b1b4998424d6cd5d5bafa9745a088815e868e7
User <user@MacBook-Air-User.local>
2020-01-30 17:48:01 +0500

    fix mysql3

--- app/Math.php
+++ app/Math.php
@@ -5,6 +5,6 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Math extends Model {
-    protected $connection = 'mysql3';
+    protected $connection = 'mysql2';
     //protected $table = 'mathexpe_kniga_resheniy';
 }
